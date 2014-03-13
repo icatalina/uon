64f8e47ac66f33d581784268f9ef89ee839fc7d1
icatalina <icatalina@gmail.com>
2014-03-13 07:30:40 +0000

    Refactor of the Code, use of Json & Properties

--- utils/twigLoader.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once 'vendor/twig/Autoloader.php';
-Twig_Autoloader::register();
-
-$loader = new Twig_Loader_Filesystem(conf('folders.input'));
-return new Twig_Environment($loader);
